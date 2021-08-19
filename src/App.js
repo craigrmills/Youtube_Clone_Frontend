1555761a261b8bc7a61154130e4853dffc910e68
craigrmills <craig.r.mills86@gmail.com>
2021-08-19 14:43:06 -0500

    Created SideBar + SideBarRow Components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import React from 'react';
 import Header from './Components/Header/Header';
-
+import SideBar from './Components/SideBar/SideBar';
+import RecommendedVideos from './Components/RecommendedVideos/RecommendedVideos';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header/>
-      {/* <Sidebar/> */}
-      {/* <RecommendedVideo/> */}
+      <div className="app__page">
+        <SideBar/>
+        <RecommendedVideos/>
+      </div>
     </div>
   );
 }
